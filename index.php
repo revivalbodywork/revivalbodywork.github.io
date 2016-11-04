41ff58fb8d41854441013c4c7c09ed18d03aae3b
revivalbodywork <trumbull.j@gmail.com>
2016-11-04 16:37:26 -0400

    Revision

--- index.php
+++ index.php
@@ -3,24 +3,23 @@
 <head>
 	<meta charset="UTF-8">
 
-	<title>Cluster - Creative Portfolio HTML Template</title>
+	<title>Revival Bodywork &middot; Message therapy</title>
 	
 	<!-- Main CSS file -->
 	<link rel="stylesheet" href="css/bootstrap.min.css" />
 	<link rel="stylesheet" href="css/owl.carousel.css" />
-	<link rel="stylesheet" href="css/magnific-popup.css" />
 	<link rel="stylesheet" href="css/font-awesome.css" />
 	<link rel="stylesheet" href="css/style.css" />
 	<link rel="stylesheet" href="css/responsive.css" />
-
 	
-	<!-- Favicon -->
+	<!-- Favicon
 	<link rel="shortcut icon" href="images/icon/favicon.png">
 	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/icon/apple-touch-icon-144-precomposed.png">
 	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/icon/apple-touch-icon-114-precomposed.png">
 	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/icon/apple-touch-icon-72-precomposed.png">
 	<link rel="apple-touch-icon-precomposed" href="images/icon/apple-touch-icon-57-precomposed.png">
-	
+	-->
+    
 	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
@@ -455,13 +454,9 @@
 	<script type="text/javascript" src="js/bootstrap.min.js"></script><!-- Bootstrap -->
 	<script type="text/javascript" src="js/jquery.parallax.js"></script><!-- Parallax -->
 	<script type="text/javascript" src="js/smoothscroll.js"></script><!-- Smooth Scroll -->
-	<script type="text/javascript" src="js/masonry.pkgd.min.js"></script><!-- masonry -->
-	<script type="text/javascript" src="js/jquery.fitvids.js"></script><!-- fitvids -->
 	<script type="text/javascript" src="js/owl.carousel.min.js"></script><!-- Owl-Carousel -->
 	<script type="text/javascript" src="js/jquery.counterup.min.js"></script><!-- CounterUp -->
 	<script type="text/javascript" src="js/waypoints.min.js"></script><!-- CounterUp -->
-	<script type="text/javascript" src="js/jquery.isotope.min.js"></script><!-- isotope -->
-	<script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script><!-- magnific-popup -->
 	<script type="text/javascript" src="js/scripts.js"></script><!-- Scripts -->
 
 
